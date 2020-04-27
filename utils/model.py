e26048494262814c3261431a4e53be21e018d459
wuxuegithub <wuxue@connect.hku.hk>
2020-04-27 23:28:44 +0800

    template

---
+++ utils/model.py
@@ -0,0 +1,13 @@
+# LeNet5 object
+class LeNet5(object):
+    def __init__(self):
+        # YOUR IMPLEMETATION
+        raise NotImplementedError
+
+    def Forward_Propagation(self, input_image, input_label, mode):
+        # YOUR IMPLEMETATION
+        raise NotImplementedError
+
+    def Back_Propagation(self, lr_global):
+        # YOUR IMPLEMETATION
+        raise NotImplementedError
